e45926e6c87af77fae56913d2cf271c9dce00296
huarong_li <huarong_li@foxitsoftware.com>
2022-09-14 11:24:13 +0800

    add file.

---
+++ CMake/Command/configure_file/input.h
@@ -0,0 +1,14 @@
+#cmakedefine VAR 3.14
+#cmakedefine VAR1 "Good"
+#cmakedefine01 VAR3
+#cmakedefine01 VAR4
+#cmakedefine NAME "@NAME@"
+#cmakedefine NEW_NAME "${NEW_NAME}"
+#cmakedefine QUOTE  "@QUOTE@"
+
+#include <iostream>
+
+void ${ my_print }() {
+	std::cout << "print something" << std::endl;
+}
+
